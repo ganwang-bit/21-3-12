40f018faa930d9b4349fc0f9edc0d0a9424bd91f
ganwang <74584251+ganwang-bit@users.noreply.github.com>
2021-03-12 20:20:32 +0800

    21-3-12

---
+++ 21-3-12cpp/main.cpp
@@ -0,0 +1,59 @@
+//#include <iostream>
+//
+//using namespace std;
+//
+//int main()
+//{
+//    cout << "Hello world!" << endl;
+//    return 0;
+//}
+//#include<iostream>
+//using namespace std;
+//#include<vector>
+//#include<algorithm>
+//#include<string>
+//class Person
+//{
+//public:
+//    Person(string name)
+//    {
+//        this->name=name;
+//    }
+//    string name;
+//};
+//int main()
+//{
+//    vector<Person>v;
+//    Person a1("1");
+//    Person a2("2");
+//    Person a3("3");
+//    Person a4("4");
+//    Person a5("5");
+//    v.push_back(a1);
+//    v.push_back(a2);
+//    v.push_back(a3);
+//    v.push_back(a4);
+//    v.push_back(a5);
+//    for(vector<Person>::iterator it=v.begin();it!=v.end();it++)
+//    {
+//        cout<<(*it).name<<endl;
+//    }
+//    return 0;
+//}
+//#include<iostream>
+//using namespace std;
+//#include<vector>
+//#include<algorithm>
+//#include<stdio.h>
+//int main()
+//{
+//    short a=0x12345678L;
+//    printf("%x",'a'-'Z');
+//    return 0;
+//}
+//#include<stdio.h>
+//int main()
+//{
+//    printf("%d",1/-2);
+//    return 0;
+//}
